b79da8a695d5b4cd70c367ee76fb4cc8767dacd4
Lingheng Meng <linghengmeng@yahoo.com>
2019-09-02 17:37:17 -0400

    fix backup value

--- spinup/algos/td3_n_step/td3_n_step.py
+++ spinup/algos/td3_n_step/td3_n_step.py
@@ -226,8 +226,11 @@ def td3_n_step(env_fn, actor_critic=core.mlp_actor_critic, ac_kwargs=dict(), see
     # Bellman backup for Q functions, using Clipped Double-Q targets
     min_q_targ = tf.minimum(q1_targ, q2_targ)
     backup = tf.stop_gradient(
-        tf.reduce_sum(tf.multiply(tf.pow(gamma, tf.range(1, n_step_ph + 1)) * (1 - d_ph), r_ph), axis=1)
+        tf.reduce_sum(tf.multiply(tf.pow(gamma, tf.range(0, n_step_ph)) * (1 - d_ph), r_ph), axis=1)
         + gamma ** n_step_ph * (1 - d_ph[:, -1]) * min_q_targ)
+    # backup = tf.stop_gradient(
+    #     tf.reduce_sum(tf.multiply(tf.pow(gamma, tf.range(1, n_step_ph + 1)) * (1 - d_ph), r_ph), axis=1)
+    #     + gamma ** n_step_ph * (1 - d_ph[:, -1]) * min_q_targ)
 
     # TD3 losses
     pi_loss = -tf.reduce_mean(q1_pi)
