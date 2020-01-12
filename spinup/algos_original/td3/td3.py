d2511863a5e359c09c94738f6fd4ec57f205732c
Lingheng Meng <linghengmeng@yahoo.com>
2020-01-12 00:42:59 -0500

    defualt 2L 300

--- spinup/algos_original/td3/td3.py
+++ spinup/algos_original/td3/td3.py
@@ -311,7 +311,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='HalfCheetah-v2')
     parser.add_argument('--hid', type=int, default=300)
-    parser.add_argument('--l', type=int, default=1)
+    parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--epochs', type=int, default=200)
